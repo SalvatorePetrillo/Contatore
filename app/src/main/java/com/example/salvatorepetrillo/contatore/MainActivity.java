814a7e38c7dbbd539f025b558f5f4d3e2b93f0e7
Salvatore Petrillo <s.petrillo96@hotmail.it>
2018-03-26 15:55:18 +0200

    La prima versione funzionante è questa non quella di prima

--- app/src/main/java/com/example/salvatorepetrillo/contatore/MainActivity.java
+++ app/src/main/java/com/example/salvatorepetrillo/contatore/MainActivity.java
@@ -49,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
      */
     private void visualizzaMessaggio ()
     {
-        vMessaggio.setText(getString(R.string.valore) " " + contatore);
+        vMessaggio.setText(getString(R.string.valore) + contatore);
     }
 
 }
